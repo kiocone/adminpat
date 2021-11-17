57651b7498c4884f8f93161e4c353360a1a4c1b4
Kiocone <kiocone@gmail.com>
2021-11-17 11:56:41 -0500

    update pull push token

--- autopull.bat
+++ autopull.bat
@@ -1,2 +1,2 @@
 cd c:\adminpat
-git pull https://kiocone:Ka*121601001@github.com/kiocone/adminpat.git
+git pull https://kiocone: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@github.com/kiocone/adminpat.git
